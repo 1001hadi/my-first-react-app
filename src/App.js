8cb7e65d4e62869558904ee2e3546033599df860
1001hadi <hadi.jaf.1001@gmail.com>
2022-09-15 17:24:05 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import HeadeUnit from "./Header";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Todo({ todo, index, completeTodo, deleteTodo}) {
+  return(
+    <div className="line">
+      <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
+        className="todo">{ todo.text }
+        <div>
+          <button onClick={() => completeTodo(index)}>Complete</button>
+          <button onClick={() => deleteTodo(index)}>X</button>
+        </div>
+      </div>
     </div>
-  );
+   
+  )
+}
+
+
+function TodoForm({addTodo}) {
+  const[value, setValue] = useState('');
+
+  const handleSubmit = e=> {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+        className="input"
+        value={value}
+        placeholder="Add todos..." 
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app">
+      <div>
+        <HeadeUnit/>
+      </div>
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} deleteTodo={deleteTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const HeadUnit = (() => {
+    return (
+        <div className="header">
+            <h1>What's Your Plan Today</h1>
+        </div>
+    )
+})
+
+export default HeadUnit;
